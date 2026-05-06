07b5d5e4fcee184372b17903619e6af44177e689
lucas-Joyce <lucasjoyce1@gmail.com>
2026-05-06 20:03:56 +0100

    feast: landingPage big visual Cards

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -2,24 +2,33 @@ import { Link } from 'react-router-dom'
 import '../styles/pages/landingPage.scss'
 
 const countries = [
-  { name: 'United Kingdom', path: '/uk' },
-  { name: 'United States', path: '/usa' },
-  { name: 'France', path: '/france' },
-  { name: 'Germany', path: '/germany' },
-  { name: 'Philippines', path: '/philippines' },
+  { name: 'United Kingdom', path: '/uk',          clr: '#cf142b' },
+  { name: 'United States',  path: '/usa',         clr: '#3c3b6e' },
+  { name: 'France',         path: '/france',      clr: '#002395' },
+  { name: 'Germany',        path: '/germany',     clr: '#ffcc00' },
+  { name: 'Philippines',    path: '/philippines', clr: '#0038a8' },
 ]
 
 const LandingPage = () => {
   return (
     <div className="landing">
       <h1 className="landing__title">Political Map</h1>
-      <ul className="landing__list">
-        {countries.map(({ name, path }) => (
-          <li key={path} className="landing__item">
-            <Link to={path} className="landing__link">{name}</Link>
-          </li>
+      <div className="landing__cards">
+        {countries.map(({ name, path, clr }) => (
+          <Link
+            key={path}
+            to={path}
+            className="card"
+            style={{ '--clr': clr }}
+          >
+            <div className="imgBx">
+              <img src="" alt="" />
+            </div>
+            <img src="" alt="" className="c3d" />
+            <h2>{name}</h2>
+          </Link>
         ))}
-      </ul>
+      </div>
     </div>
   )
 }
